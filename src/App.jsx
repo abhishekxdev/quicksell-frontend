4e22fd94f8fcfe18c7b8c3f78237a5ead713c3fe
abhishekxdev <139157548+abhishekxdev@users.noreply.github.com>
2024-10-22 16:32:06 +0530

    adding base files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable react-hooks/exhaustive-deps */
+import  { useCallback, useEffect, useState } from 'react';
+import Header from './components/Header';
+import Grid from './components/Grid';
+import { GET_TICKETS_URL } from './constants';
+import { loadGrid, mapUsersByUserId } from './utils';
+
+import Loader from './components/Loader';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tickets, setTickets] = useState([]);
+  const [userData, setUserData] = useState({});
+  const [gridData, setGridData] = useState({});
+  const [grouping, setGrouping] = useState("status");
+  const [ordering, setOrdering] = useState("priority");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadSettings();
+    fetch(GET_TICKETS_URL).then(resp => resp.json()).then(res => {
+      const { tickets, users } = res;
+      setTickets(tickets);
+      setUserData(mapUsersByUserId(users));
+    })
+  }, [])
+
+  useEffect(() => {
+    if (!tickets.length)
+      return;
+    setGridData(loadGrid(tickets, grouping, ordering));
+    setLoading(false);
+  }, [grouping, ordering, tickets])
+
+  const onSetGrouping = useCallback((value) => {
+    setLoading(true);
+    setGrouping(value);
+    saveSettings({ grouping: value });
+  }, []);
+
+  const onSetOrdering = useCallback((value) => {
+    setLoading(true);
+    setOrdering(value);
+    saveSettings({ ordering: value });
+  }, []);
+
+  const saveSettings = useCallback((data) => {
+    for (let key in data)
+      localStorage.setItem(key, data[key]);
+  }, []);
+
+  const loadSettings = useCallback(() => {
+    setGrouping(localStorage.getItem("grouping") || "status");
+    setOrdering(localStorage.getItem("ordering") || "priority");
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Header grouping={grouping} setGrouping={onSetGrouping} ordering={ordering} setOrdering={onSetOrdering} />
+      {loading ? <Loader /> :
+        <Grid gridData={gridData} grouping={grouping} userIdToData={userData} />
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
